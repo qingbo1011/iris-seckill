bddcfffeada8978d5caa521ea5b47c3c8316411f
qingbo1011 <61835535+qingbo1011@users.noreply.github.com>
2022-08-01 10:06:54 +0800

    成功添加商品

--- backend/web/controller/product.go
+++ backend/web/controller/product.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"iris-seckill/model"
+	"iris-seckill/request"
 	"iris-seckill/service"
 
 	"github.com/kataras/iris/v12"
@@ -43,5 +44,25 @@ func (p *ProductController) GetAdd() mvc.View {
 	return mvc.View{
 		Name: "product/add.html",
 	}
+}
 
+// PostAdd 添加商品（POST请求去向数据库加数据）
+func (p *ProductController) PostAdd() {
+	productReq := request.ProductReq{}
+	err2 := p.Ctx.ReadForm(&productReq)
+	if err2 != nil {
+		p.Ctx.StopWithError(iris.StatusBadRequest, err2)
+		return
+	}
+	product := model.Product{
+		ProductName:     productReq.ProductName,
+		ProductNum:      productReq.ProductNum,
+		ProductImageUrl: productReq.ProductImageUrl,
+		ProductUrl:      productReq.ProductUrl,
+	}
+	_, err := p.ProductService.InsertProduct(&product)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	p.Ctx.Redirect("/product/all")
 }
