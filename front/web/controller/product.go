2b4359e066291761cd35035b257e62fea3763b51
qingbo1011 <61835535+qingbo1011@users.noreply.github.com>
2022-08-01 21:51:01 +0800

    GetOrder 获取下订单页面

--- front/web/controller/product.go
+++ front/web/controller/product.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"iris-seckill/model"
 	"iris-seckill/service"
+	"strconv"
 
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/mvc"
@@ -17,7 +19,6 @@ type ProductController struct {
 
 // GetDetail 获取商品详情信息页面
 func (p *ProductController) GetDetail() mvc.View {
-
 	product, err := p.ProductService.GetProductByID(2) // 这里product_id写死了
 	if err != nil {
 		p.Ctx.Application().Logger().Error(err)
@@ -27,6 +28,58 @@ func (p *ProductController) GetDetail() mvc.View {
 		Name:   "product/view.html",
 		Data: iris.Map{
 			"product": product,
+		},
+	}
+}
+
+// GetOrder 获取下订单页面
+func (p *ProductController) GetOrder() mvc.View {
+	productString := p.Ctx.URLParam("productID")
+	userString := p.Ctx.GetCookie("uid")
+	productID, err := strconv.Atoi(productString)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	product, err := p.ProductService.GetProductByID(uint(productID))
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+
+	var orderID uint
+	showMessage := "抢购失败！"
+	// 判断商品数量是否满足需求
+	if product.ProductNum > 0 {
+		// 扣除商品数量
+		product.ProductNum = product.ProductNum - 1
+		err := p.ProductService.UpdateProduct(product)
+		if err != nil {
+			p.Ctx.Application().Logger().Debug(err)
+		}
+		// 创建订单
+		userID, err := strconv.Atoi(userString)
+		if err != nil {
+			p.Ctx.Application().Logger().Debug(err)
+		}
+		order := &model.Order{
+			UserID:      uint(userID),
+			ProductID:   uint(productID),
+			OrderStatus: model.OrderSuccess,
+		}
+		// 新建订单
+		orderID, err = p.OrderService.InsertOrder(order)
+		if err != nil {
+			p.Ctx.Application().Logger().Debug(err)
+		} else {
+			showMessage = "抢购成功！"
+		}
+	}
+
+	return mvc.View{
+		Layout: "shared/productLayout.html",
+		Name:   "product/result.html",
+		Data: iris.Map{
+			"orderID":     orderID,
+			"showMessage": showMessage,
 		},
 	}
 }
